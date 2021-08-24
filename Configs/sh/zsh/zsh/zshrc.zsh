ea41110e58dc5fc1d67c06bc5ef43856c18d70ba
Atai Ambus <atayambus+org@gmail.com>
2021-08-24 23:16:41 +0300

    ref(zsh/rc): add _source function to handle file not found
    Detects if a source file does not exist and prints an appropriate warning.

--- Configs/sh/zsh/zsh/zshrc.zsh
+++ Configs/sh/zsh/zsh/zshrc.zsh
@@ -1,9 +1,17 @@
 
 
-[ -f ~/Gray/CONFIG/Configs/sh/sh/confdirs.sh ] && source ~/Gray/CONFIG/Configs/sh/sh/confdirs.sh
+_source() {
+    file_path="$1"
+    if [ -f "$file_path" ]; then
+        source "$file_path"
+    else
+        echo "could not find basic config: $file_path. Aborting ~/.zshrc"
+    fi
+}
 
-[ -f  "$OMZ_DIR/custom/omzrc.zsh.init" ] && source "$OMZ_DIR/custom/omzrc.zsh.init"
-[ -f "$SH_DIR/profile" ] && source "$SH_DIR/profile"
+_source ~/Gray/CONFIG/Configs/sh/sh/confdirs.sh
+_source "$ZSH_DIR/pluginsrc.zsh"
+_source "$SH_DIR/profile"
 
 # Title Congfiguration
 precmd () { print -Pn "\e]0;Zsh ($ZSH_ID)\a" }
