02c9541d3b8261b9056c33970459937713f75d29
Atai Ambus <atayambus@gmail.com>
2021-08-24 23:25:29 +0300

    add aliases for android apps

---
+++ Configs/sh/sh/app_aliases.sh
@@ -0,0 +1,61 @@
+comm="$comm"
+
+telegram="org.telegram.messenger/org.telegram.ui.LaunchActivity"
+whatsapp="com.whatsapp/.Main"
+sms='com.google.android.apps.messaging/.ui.ConversationListActivity'
+
+firefox='org.mozilla.fenix/.App'
+spotify='com.spotify.music/.MainActivity'
+docs='om.google.android.apps.docs.editors.docs/com.google.android.apps.docs.app.NewMainProxyActivity'
+
+toothbrush="enouplus.enouhealth.smilelite/.SplashActivity"
+
+dominos= 'il.co.dominos.android/.MainActivity'
+idf='com.ng.malshab.malshab/.ui.MainActivity'
+
+alias msg="$comm $telegram"
+alias t="$comm $telegram"
+alias ט="$comm $telegram"
+
+alias w="$comm $whatsApp"
+alias ו="$comm $whatsApp"
+
+alias yt="$comm com.vanced.android.youtube/com.google.android.youtube.app.honeycomb.Shell\$HomeActivity"
+
+alias gui="$comm com.ss.launcher2/.MainActivity"
+
+alias conf="$comm com.android.settings/.Settings"
+
+alias files="$comm me.zhanghai.android.files/.filelist.FileListActivity"
+
+alias brush="$comm $toothbrush"
+alias tth="$comm $toothbrush"
+alias שן="$comm $toothbrush"
+
+alias p="$comm com.sec.android.app.camera/.Camer"
+
+alias g="$comm com.google.android.apps.photos/.home.HomeActivity"
+
+alias slp="$comm com.urbandroid.sleep.full.key/.MainActivity"
+
+alias s="$comm $spotify"
+alias ס="$comm $spotify"
+
+alias pizza="$comm $dominos"
+alias dominos="$comm $dominos"
+alias דומינוס="$comm $dominos"
+
+alias pls="$comm com.android.vending/.AssetBrowserActivity"
+alias ca="$comm com.google.android.calendar/com.android.calendar.AllInOneActivity"
+
+alias docs="$comm $docs"
+alias דוקס="$comm $docs"
+
+alias e="$comm $firefox"
+alias א="$comm $firefox"
+
+alias idf="$comm $idf"
+alias צהל="$comm $idf"
+
+alias sms="$comm $sms"
+alias סמס="$comm $sms"
