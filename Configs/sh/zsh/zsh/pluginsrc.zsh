b5dd780205ef2d9f05c31336c0d307cf0e9ce42f
Atai Ambus <atayambus+org@gmail.com>
2021-08-24 23:14:47 +0300

    ref(zsh): add pluginsrc to handle plugins
    pluginsrc intializes oh-my-zsh as well as any pther external plugins or packages (currently amounting to command-not-found).

---
+++ Configs/sh/zsh/zsh/pluginsrc.zsh
@@ -0,0 +1,13 @@
+
+_source_package() {
+    package_name="$1"
+    file_path="$2"
+    if [ -f "$file_path" ]; then
+        source "$file_path"
+    else
+        echo "could not find $(basename "$file_path") for package $package_name: $file_path is not a file."
+    fi
+}
+
+_source_package 'command-not-found' '/etc/zsh_command_not_found'
+_source_package oh-my-zsh "$OMZ_DIR/custom/omzrc.zsh.init"
