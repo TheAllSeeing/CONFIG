c2c1c7ff4b2b28ade678480866335a80ca1d0819
Atai Ambus <atayambus@gmail.com>
2021-08-25 19:13:02 +0300

    feat(sh/local): carefuly unalias function names.
    Since aliases cause errors when defining functions, unaliasing may be
    needed when other configuration files aliasweith function names.
    However, if an alias was not created, trying to unalias it will causecan
    error.
    
    __unalias removes existing input aliases and proceeds if an alias does
    not exist. I ran it befire each function to make sure errors are cleaned.

--- Configs/sh/sh/local.sh
+++ Configs/sh/sh/local.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+__unalias() {
+    if alias $1 >/dev/null 2>&1; then
+        unalias $1
+    fi
+}
 # Localization Standartization parameters: function, directory pointers, path additions
 # DEVICE: AllSeer (Desktop Linux System)
 
@@ -17,25 +22,31 @@ _ls() {
 }
 
 # sudo or sudo equivalent pointer (for cygwin, android, etc.)
+__unalias sudo
 sudo() {
     echo "Phone not rooted, cannot sudo." >2
     exit 1
 }
 
 # xdg-open or equivalent pointer
+
+__unalias open
 open() {
-    xdg-open "$@"
+    tel-open "$@"
 }
 
 # Open a url link
+__unalias url
 url() {
     termux-open-url $(echo "$@" | sed "s/ /%20/g")
 }
 
+__unalias xcopy
 xcopy() {
     /data/data/com.termux/files/home/.local/bin/xcopy $@
 }
 
+__unalias xpaste
 xpaste() {
     /data/data/com.termux/files/home/.local/bin/xpaste $@ bs
 }
