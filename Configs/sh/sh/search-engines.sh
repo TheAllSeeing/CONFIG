f55c68eaf14698e314066278e45d411f5501bf1d
Atai Ambus <atayambus+org@gmail.com>
2021-08-29 16:17:17 +0300

    fix(sh/engines): remove incorrect then/else semicolon
    then and else acts as a kind of command similar to eval or time and run their input conditionally, kind of
    
    I tried to run `if condition; then; command; else; command; fi`. It should be without semicolons after `then` and `else`, like `if condition; then command; else command; fi`

--- Configs/sh/sh/search-engines.sh
+++ Configs/sh/sh/search-engines.sh
@@ -18,7 +18,7 @@ _def_search_engine() {
     addr="$2"
     search_addr="$2/$3"
 
-    eval "$engine_name() { if [[ -z \"\$@\" ]]; then; echo opening $addr; url $addr; else; sed_str=\"s/%s/\$@/\"; url \"\$(echo '$search_addr' | sed \"\$sed_str\")\"; fi }"
+    eval "$engine_name() { if [[ -z \"\$@\" ]]; then echo opening $addr; url $addr; else sed_str=\"s/%s/\$@/\"; url \"\$(echo '$search_addr' | sed \"\$sed_str\")\"; fi }"
 
     # $engine_name() {
     #     if [[ -z "\$@" ]]; then
