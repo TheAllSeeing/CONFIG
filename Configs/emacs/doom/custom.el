3627d55fd3e4d0990307c3a0691910f13ce17611
Atai Ambus <atayambus+org@gmail.com>
2021-09-05 13:06:25 +0300

    feat(emacs): install yaml package

--- Configs/emacs/doom/custom.el
+++ Configs/emacs/doom/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files (quote ("/home/atai/Code-Steel (ORG)/ACCESS/Agenda")))
- '(package-selected-packages (quote (org-bullets company-tabnine projectile))))
+ '(package-selected-packages (quote (yaml org-bullets company-tabnine projectile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
